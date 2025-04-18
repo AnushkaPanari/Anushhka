f8bd209f22ba865a56bebf72623c722a79c1fc96
AnushkaPanari <panarianushka0@gmail.com>
2025-04-18 20:20:05 +0530

    Update helloprogram.java

--- helloprogram.java
+++ helloprogram.java
@@ -1,7 +1,7 @@
 class helloprogram
   {
-    public static viod main(String[]args)
+    public static void main(String[]args)
     {
-      System.out.println("hello program")
-        }
+      System.out.println("hello program");
+    }
   }
